a7c392d17d2eedf269b83791f73825e0d3443396
jglatts <john@z-axiscc.com>
2026-04-13 11:51:16 -0400

    start working on mitutoyo gauge impl

---
+++ Wedge/MitutoyoSerialReader.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.IO.Ports;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Wedgies;
+
+namespace Wedgies
+{
+    public class MitutoyoSerialReader : SerialReaderBase
+    {
+        public MitutoyoSerialReader(SerialPort port, UpdateCallback callback) : 
+            base(port, callback)
+        {
+        }
+
+    }
+}
